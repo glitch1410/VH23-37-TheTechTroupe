ca028349247b49e5723480c67758fbe1398fd3b0
Prathamesh Yandolli <101892344+glitch1410@users.noreply.github.com>
2023-10-06 16:44:43 +0530

    Add files via upload

---
+++ .github/Whack-a-Word/mole.js
@@ -0,0 +1,20 @@
+let currentMoleTile;
+
+
+window.onload=function() {
+        SetGame();
+
+}
+
+function SetGame(){
+    for(let i= 0; i<9; i++) {
+            let tile = document.createElement("div");
+            tile.id= i.toString();
+            document.getElementById("board").appendChild(tile); 
+    }
+
+function setMole(){
+    let mole = document.createElement("img");
+	<p>hello</p> 
+}
+}
